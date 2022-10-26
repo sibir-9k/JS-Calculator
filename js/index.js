cb8bb9d529db6ca7298ef7d6a77a74b1de3f9ef4
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2022-10-26 22:58:59 +0300

    added basic math operations

--- js/index.js
+++ js/index.js
@@ -1,10 +1,78 @@
-const number = document.querySelectorAll('.number')
-const operator = document.querySelectorAll('.operator')
-const prevOperand = document.getElementsByClassName('previous-operand')
-const currentOperand = document.getElementsByClassName('current-operand')
+const allNumber = document.querySelectorAll(".number");
+const allOperator = document.querySelectorAll(".operator");
+const prevOperand = document.getElementById("prevOperand");
+const currentOperand = document.getElementById("currentOperand");
+const clearBtn = document.getElementById("clear");
+const equals = document.getElementById("equals");
 
-for(let elem of number){
-  elem.addEventListener('click', function(e){
-    console.log(e.target.innerText)
-  })
-}
+let prevNum = "";
+let currentNum = "";
+let haveDot = false;
+let result = null;
+let lastOperator = "";
+
+allNumber.forEach((number) => {
+	number.addEventListener("click", (e) => {
+		if (e.target.innerText === "." && !haveDot) {
+			haveDot = true;
+		} else if (e.target.innerText === "." && haveDot) {
+			return;
+		}
+		currentNum += e.target.innerText;
+		currentOperand.innerText = currentNum;
+	});
+});
+
+allOperator.forEach((operator) => {
+	operator.addEventListener("click", (e) => {
+		if (!currentNum) return;
+		haveDot = false;
+		const operatorName = e.target.innerText;
+		if (prevNum && currentNum && lastOperator) {
+			mathOperation();
+		} else {
+			result = parseFloat(currentNum);
+		}
+		clearVar(operatorName);
+		lastOperator = operatorName;
+	});
+});
+
+const clearVar = (num = "") => {
+	prevNum += currentNum + " " + num + " ";
+	prevOperand.innerText = prevNum;
+	currentOperand.innerText = "";
+	currentNum = "";
+};
+
+const mathOperation = () => {
+	if (lastOperator === "+") {
+		result = parseFloat(prevNum) + parseFloat(currentNum);
+	} else if (lastOperator === "-") {
+		result = parseFloat(result) - parseFloat(currentNum);
+	} else if (lastOperator === "/") {
+		result = parseFloat(result) / parseFloat(currentNum);
+	} else if (lastOperator === "×") {
+		result = parseFloat(result) * parseFloat(currentNum);
+	} else if (lastOperator === "%") {
+		result = parseFloat(result) % parseFloat(currentNum);
+	}
+};
+
+equals.addEventListener("click", () => {
+	if (!prevNum || !currentNum) return;
+	haveDot = false;
+	mathOperation();
+	clearVar();
+	currentOperand.innerText = result;
+	currentNum = result;
+	// prevOperand.innerText = "";
+});
+
+clearBtn.addEventListener("click", () => {
+	prevOperand.innerText = 0;
+	currentOperand.innerText = 0;
+	prevNum = "";
+	currentNum = "";
+	result = null;
+});
